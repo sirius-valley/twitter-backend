cd523cebcd21ad6efc172da886338d98ad64257d
ron2452 <matias.nahuel.pizzi@hotmail.com>
2024-04-19 15:21:36 -0300

    Add include statement to get author information in getPosts method

--- node/express-prisma-typescript/src/domains/post/repository/post.repository.impl.ts
+++ node/express-prisma-typescript/src/domains/post/repository/post.repository.impl.ts
@@ -31,6 +31,9 @@ export class PostRepositoryImpl implements PostRepository {
           id: 'asc'
         }
       ]
+      // include: {
+      //   author: true
+      // }
     })
     return posts.map(post => new PostDTO(post))
   }
